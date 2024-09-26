ede0501211334d4ce5bc4ed9a644704d4a95b57e
Alexa404NL <66581333+Alexa404NL@users.noreply.github.com>
2024-09-26 11:49:52 -0600

    files setup

--- linked_list.h
+++ linked_list.h
@@ -5,268 +5,249 @@
 #include <iostream>
 using namespace std;
 
+//nodes
 template <class T>
-class node
-{
-public:
-    node(T data);
-    T getData();
-    node<T> *getNext();
-    void setData(T data);
-    void setNext(node<T> *next);
-
-private:
-    T data;
-    node<T> *next;
-};
-
-template <class T>
-class LinkedList
-{
-    node<T> *head;
-
-private:
-    int size;
-
-public:
-    LinkedList()
+    class node
     {
-        head = nullptr;
-        size = 0;
-    }
-    void imprimeLinkedList();
-    int longitud();
-    void insertAtBeginning(T value);
-    void insertAtEnd(T value);
-    void insertPos(int pos, T value);
-    void removeFirstNode();
-    void removeLastNode();
-    void deleteAtPosition(int position);
-};
+    public:
+        //node
+        node(T data);
+        //getters
+        T getData();
+        node<T> *getNext();
+        node<T> *getPrevious();
+        //setters
+        void setData(T data);
+        void setNext(node<T> *next);
+        void setPrevious(node<T> *previous);
+
+    private:
+        T data;
+        node<T> *next;
+        node<T> *previous;
+    };
+
+//list
+
+template <class T>
+    class DoublyLinkedList
+    {
+        node<T> *head;
+        node<T> *tail;
+    private:
+        int size;
+
+    public:
+        //contructor
+        DoublyLinkedList(){
+            head = nullptr;
+            tail = nullptr;
+            size = 0;
+        }
+
+        //getter
+        int longitud();
+
+        // setters
+        void imprimeLinkedList();
+        void insertAtBeginning(T value);
+        void insertAtEnd(T value);
+        void insertPos(int pos, T value);
+        void removeFirstNode();
+        void removeLastNode();
+        void deleteAtPosition(int position);
+    };
 
 // definiciones de la clase node.
 
 template <class T>
-inline node<T>::node(T data)
-{
-    this->data = data;
-    this->next = nullptr;
-}
-
-template <class T>
-inline node<T> *node<T>::getNext()
-{
-    if (next != nullptr)
-        return next;
-    else
-        return nullptr;
-}
-
-template <class T>
-inline T node<T>::getData()
-{
-    return data;
-}
-
-template <class T>
-inline void node<T>::setData(T data)
-{
-    this->data = data;
-}
-
-template <class T>
-inline void node<T>::setNext(node<T> *next)
-{
-    this->next = next;
-}
-
-// fin definiciones de la clase node.
-
-// definiciones de la clase LinkedList
-
-/* Operaciones de la lista a implementarse
-
-*Agregar: agrega un elemento a la sucesión. El agregar para las listas enlazadas se realiza en 3 posiciones, al principio, al final y en otra posición intermedia.
-*Eliminar: elimina un elemento de la sucesión (no actúa con la sucesión vacía). Eliminar para las listas enlazadas se realiza en 3 posiciones, al principio, al final y en otra posición intermedia.
-Consultar: devuelve un elemento de la sucesión de acuerdo a una posición dada como dato (no actúa con la sucesión vacía)
-Vacia: nos dice si está vacía o no. Que una lista esté vacía significa que el puntero al primer elemento es NULO.
-Vaciar: Elimina todos los elementos de una lista.
-*longitud: Nos dice el tamaño o cantidad de elementos que contiene la lista.  (Longitud())
-Concatenar: Unir dos listas.
-Invertir: Invierte el orden de los elementos de la lista.
-Ordenar: Ordena por algún criterio los elementos de la lista.
-Insertar en Orden: Inserta en la posición que garantice un orden predefinido. La lista siempre estará ordenada.
-intercambiar: intercambia dos elementos de la lista.
-*imprime: Visualiza los elementos de la lista.
-
-*/
-
-template <class T>
-int LinkedList<T>::longitud()
-{
-    return size;
-}
-
-template <class T>
-void LinkedList<T>::imprimeLinkedList()
-{
-    // Se comienza por el nodo de inicio
-    node<T> *current = head;
-    if (head == nullptr)
-    {
-        cout << "Lista vacia.." << endl;
-        return;
-    }
-    // recorre la lista hasta el final.
-    while (current != nullptr)
-    {
-
-        // Visualiza el dato
-        cout << current->getData() << " ";
-
-        // Siguiente nodo
-        current = current->getNext();
+    inline node<T>::node(T data){
+        this->data = data;
+        this->next = nullptr;
     }
 
-    cout << std::endl;
-}
+template <class T>
+    inline node<T> *node<T>::getNext(){
+        if (next != nullptr)
+            return next;
+        else
+            return nullptr;
+    }
 
 template <class T>
-void LinkedList<T>::insertAtBeginning(T value)
-{
-    node<T> *newNode = new node<T>(value);
-    newNode->setNext(head);
-    head = newNode;
-    size++;
-}
+    inline T node<T>::getData(){
+        return data;
+    }
 
 template <class T>
-void LinkedList<T>::insertAtEnd(T value)
-{
-    node<T> *newNode = new node<T>(value);
+    inline void node<T>::setData(T data){
+        this->data = data;
+    }
 
-    // Caso lista vacía
-    if (head == nullptr)
+template <class T>
+    inline void node<T>::setNext(node<T> *next){
+        this->next = next;
+    }
+
+
+template <class T>
+    int DoublyLinkedList<T>::longitud(){
+        return size;
+    }
+
+template <class T>
+    void DoublyLinkedList<T>::imprimeLinkedList(){
+        // Se comienza por el nodo de inicio
+        node<T> *current = head;
+        if (head == nullptr)
+        {
+            cout << "Lista vacia.." << endl;
+            return;
+        }
+        // recorre la lista hasta el final.
+        while (current != nullptr)
+        {
+
+            // Visualiza el dato
+            cout << current->getData() << " ";
+
+            // Siguiente nodo
+            current = current->getNext();
+        }
+
+        cout << std::endl;
+    }
+
+template <class T>
+    void DoublyLinkedList<T>::insertAtBeginning(T value){
+        node<T> *newNode = new node<T>(value);
+        newNode->setNext(head);
         head = newNode;
-
-    // Recorrer la lista hasta el nodo final...
-    node<T> *current = head;
-    while (current->getNext() != nullptr)
-    {
-        current = current->getNext();
+        size++;
     }
-
-    // el puntero current está apuntando al nodo final.
-    current->setNext(newNode);
-    size++;
-}
 
 template <class T>
-void LinkedList<T>::insertPos(int pos, T value)
-{
-    if (pos < 1 || pos > size)
-    {
-        cout << "Posicion no permitida" << endl;
-        return;
-    }
+    void DoublyLinkedList<T>::insertAtEnd(T value){
+        node<T> *newNode = new node<T>(value);
 
-    // Insertar al principio
-    if (pos == 1)
-    {
-        insertAtBeginning(value);
-        return;
-    }
+        // Caso lista vacía
+        if (head == nullptr)
+            head = newNode;
 
-    // Recorrer la lista para encontrar el nodo anterior
-    // al punto de inserción
-    node<T> *prev = head;
-    int count = 1;
-    while (count < pos - 1 && prev != nullptr)
-    {
-        prev = prev->getNext();
-        count++;
+        // Recorrer la lista hasta el nodo final...
+        node<T> *current = head;
+        while (current->getNext() != nullptr)
+        {
+            current = current->getNext();
+        }
+
+        // el puntero current está apuntando al nodo final.
+        current->setNext(newNode);
+        size++;
     }
-    // Insertar
-    node<T> *temp = new node<T>(value);
-    temp->setNext(prev->getNext());
-    prev->setNext(temp);
-    size++;
-}
 
 template <class T>
-void LinkedList<T>::removeFirstNode()
-{
-    if (head == nullptr)
-        return;
+    void DoublyLinkedList<T>::insertPos(int pos, T value){
+        if (pos < 1 || pos > size)
+        {
+            cout << "Posicion no permitida" << endl;
+            return;
+        }
 
-    node<T> *temp = head;
-    head = head->getNext();
-    delete temp;
-    size--;
-}
+        // Insertar al principio
+        if (pos == 1)
+        {
+            insertAtBeginning(value);
+            return;
+        }
 
-template <class T>
-void LinkedList<T>::removeLastNode()
-{
-    if (head == nullptr)
-        return;
-
-    // caso de un elemento..
-    if (head->getNext() == nullptr)
-    {
-        delete head;
-        size--;
-        return;
+        // Recorrer la lista para encontrar el nodo anterior
+        // al punto de inserción
+        node<T> *prev = head;
+        int count = 1;
+        while (count < pos - 1 && prev != nullptr)
+        {
+            prev = prev->getNext();
+            count++;
+        }
+        // Insertar
+        node<T> *temp = new node<T>(value);
+        temp->setNext(prev->getNext());
+        prev->setNext(temp);
+        size++;
     }
 
-    // ir hasta el penuntimo elemento.
-    node<T> *second_last = head;
-    while ((second_last->getNext())->getNext() != nullptr)
-        second_last = second_last->getNext();
-
-    // Borrar el ultimo
-    delete second_last->getNext();
-
-    // el penultimo ahora es el último
-    second_last->setNext(nullptr);
-    size--;
-}
-
 template <class T>
-void LinkedList<T>::deleteAtPosition(int position)
-{
-    // Si la lista esta vacia o la posición no es valida
-    if (head == nullptr || position < 1 || position > size)
-    {
-        return;
-    }
+    void DoublyLinkedList<T>::removeFirstNode(){
+        if (head == nullptr)
+            return;
 
-    // If the head needs to be deleted
-    if (position == 1)
-    {
         node<T> *temp = head;
         head = head->getNext();
         delete temp;
         size--;
     }
 
-    // recorrer lista hasta el nodo anterior al que se borrará
-    node<T> *current = head;
-    for (int i = 1; i < position - 1 && current != nullptr; i++)
-    {
-        current = current->getNext();
+template <class T>
+    void DoublyLinkedList<T>::removeLastNode() {
+        if (head == nullptr)
+            return;
+
+        // caso de un elemento..
+        if (head->getNext() == nullptr)
+        {
+            delete head;
+            size--;
+            return;
+        }
+
+        // ir hasta el penuntimo elemento.
+        node<T> *second_last = head;
+        while ((second_last->getNext())->getNext() != nullptr)
+            second_last = second_last->getNext();
+
+        // Borrar el ultimo
+        delete second_last->getNext();
+
+        // el penultimo ahora es el último
+        second_last->setNext(nullptr);
+        size--;
     }
 
-    // Almacenar direccion nodo a borrar.
-    node<T> *temp = current->getNext();
+template <class T>
+    void DoublyLinkedList<T>::deleteAtPosition(int position) {
+        // Si la lista esta vacia o la posición no es valida
+        if (head == nullptr || position < 1 || position > size)
+        {
+            return;
+        }
 
-    // Update the links to bypass the node to be deleted
-    current->setNext(current->getNext()->getNext());
+        // If the head needs to be deleted
+        if (position == 1)
+        {
+            node<T> *temp = head;
+            head = head->getNext();
+            delete temp;
+            size--;
+        }
 
-    // Delete the node
-    delete temp;
-    size--;
-}
+        // recorrer lista hasta el nodo anterior al que se borrará
+        node<T> *current = head;
+        for (int i = 1; i < position - 1 && current != nullptr; i++)
+        {
+            current = current->getNext();
+        }
+
+        // Almacenar direccion nodo a borrar.
+        node<T> *temp = current->getNext();
+
+        // Update the links to bypass the node to be deleted
+        current->setNext(current->getNext()->getNext());
+
+        // Delete the node
+        delete temp;
+        size--;
+    }
 
 
 #endif //DOUBLE_LINKED_LIST_LINKED_LIST_H
